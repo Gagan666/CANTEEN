60db772241673d8a7664a03b3ba252b24385d5a3
anusha-shanbhog <anushapshanbhog@gmail.com>
2022-10-18 16:48:45 +0530

    login/signup

---
+++ Frontend/my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// styling
+import './App.css';
+
+// components 
+import Container from './components/Container';
+
+const App = () => {
+  return (
+    <div className="App cfb">
+      <Container/>
+    </div>
+  );
+}
+
+export default App;
